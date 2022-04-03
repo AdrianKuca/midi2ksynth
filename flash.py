d6247cbffaf99f4b0bf1a00fc615cd890fc887d6
Adrian Kuca <adriankuca2@gmail.com>
2022-04-03 20:37:43 +0200

    Flash with reset button

--- flash.py
+++ flash.py
@@ -1,14 +1,32 @@
 import subprocess
 from time import sleep
+import usb.core
+import usb.util
 
 AVRDUDE = "D:\\avrdude\\avrdude.exe"
-print("RESET IT!")
-print("3")
-sleep(1)
-print("2")
-sleep(1)
-print("1")
-sleep(0.5)
-print("GO!")
-subprocess.run([AVRDUDE, "-p", "atmega32u4", "-P",
-               "usb", "-D", "-U", "flash:w:midi2ksynth.hex"])
+vid = 0x03EB
+pid = 0x2048
+# dev = usb.core.find(find_all=1)
+# for cfg in dev:
+#     print(cfg)
+# if dev is not None:
+#     print("Found board: resetting")
+#     dev = usb.core.find(idVendor=vid, idProduct=pid)
+#     print(dev)
+#     # dev.ctrl_transfer(usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE, 0x01)
+#     sleep(5)
+
+subprocess.run(
+    [
+        AVRDUDE,
+        "-p",
+        "atmega32u4",
+        "-c",
+        "avr109",
+        "-P",
+        "usb:2341:0036",
+        "-D",
+        "-U",
+        "flash:w:midi2ksynth.hex",
+    ]
+)
