a7ae28a3653e5a3d3cad2c2f2c8ae98a13ccb740
Adrian Kuca <adrian.kuca@flytronic.pl>
2022-04-04 10:57:47 +0200

    Fix flashing on ubuntu

--- flash.py
+++ flash.py
@@ -2,19 +2,25 @@ import subprocess
 from time import sleep
 import usb.core
 import usb.util
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    AVRDUDE = "avrdude"
+    DEVICE = "/dev/ttyACM0"
+elif platform == "win32":
+    AVRDUDE = "D:\\avrdude\\avrdude.exe"
+    DEVICE = "usb:2341:0036"
 
-AVRDUDE = "D:\\avrdude\\avrdude.exe"
-vid = 0x03EB
-pid = 0x2048
-# dev = usb.core.find(find_all=1)
-# for cfg in dev:
-#     print(cfg)
-# if dev is not None:
-#     print("Found board: resetting")
-#     dev = usb.core.find(idVendor=vid, idProduct=pid)
-#     print(dev)
-#     # dev.ctrl_transfer(usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE, 0x01)
-#     sleep(5)
+
+vid = 0x2341
+pid = 0x0036
+dev = usb.core.find(idVendor=vid, idProduct=pid)
+
+if dev is not None:
+    print("Found board: resetting")
+    dev = usb.core.find(idVendor=vid, idProduct=pid)
+    print(dev)
+    dev.ctrl_transfer(usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE, 0x01)
+    sleep(5)
 
 subprocess.run(
     [
@@ -24,7 +30,7 @@ subprocess.run(
         "-c",
         "avr109",
         "-P",
-        "usb:2341:0036",
+        DEVICE,
         "-D",
         "-U",
         "flash:w:midi2ksynth.hex",
