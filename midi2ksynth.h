3ba3dd4a9ba4146fdaf2d007f255c89bc884f907
Adrian Kuca <adrian.kuca@flytronic.pl>
2022-10-10 13:40:07 +0200

    Remove led stuff

--- midi2ksynth.h
+++ midi2ksynth.h
@@ -47,22 +47,10 @@
 #include "Descriptors.h"
 #include "Config/AppConfig.h"
 
-#include <LUFA/Drivers/Board/LEDs.h>
 #include <LUFA/Drivers/USB/USB.h>
 #include <LUFA/Platform/Platform.h>
 
 /* Macros: */
-/** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
-#define LEDMASK_USB_NOTREADY LEDS_LED1
-
-/** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
-#define LEDMASK_USB_ENUMERATING (LEDS_LED2 | LEDS_LED3)
-
-/** LED mask for the library LED driver, to indicate that the USB interface is ready. */
-#define LEDMASK_USB_READY (LEDS_LED2 | LEDS_LED4)
-
-/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
-#define LEDMASK_USB_ERROR (LEDS_LED1 | LEDS_LED3)
 
 /** Scale factor used to convert the floating point frequencies and ratios into a fixed point number */
 #define SCALE_FACTOR 65536
